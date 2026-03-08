787ee4aa947eef9304e98d90b4bcaaedc25fdfe6
Prince <ana90kaa@gmail.com>
2026-03-08 12:15:36 +0100

    Testing snyk for node.js app

--- test3.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# Get user input for a file list
-filename = input("Enter the filename to list: ")
-
-# Vulnerable to command injection if user input is not sanitized
-os.system(f"ls -l {filename}")
